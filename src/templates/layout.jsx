5f5b270e54e7cc6a4df29eaa65e4c3a0767fec97
OskarMantilla21 <137963398+OskarMantilla21@users.noreply.github.com>
2023-11-20 22:20:46 -0500

    feat-header-menu-subheader

--- src/templates/layout.jsx
+++ src/templates/layout.jsx
@@ -1,18 +1,19 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import {Box, CssBaseline} from '@mui/material';
 
+function Layout({children}) {
+    const [header, Menu, Subheader] = children;
 
-function Layout() {
     return (
         <Box style={{display: "flex", height: "100vh"}}>
             <CssBaseline />
             <Grid container sx={{flexGrow: 1}}>
-                <Grid item flexBasis={70}>
-                    MENU
+                <Grid item flexBasis={57}>
+                    {Menu}
                 </Grid>
                 <Grid item flex={1}>
-                    <Grid xs={12}>HEADER</Grid>
-                    <Grid xs={12}>BRANDING</Grid>
+                    <Grid xs={12}>{header}</Grid>
+                    <Grid xs={12}>{Subheader}</Grid>
                     <Grid container spacing={3}>
                         <Grid xs={3}>PKI</Grid>
                         <Grid xs={3}>PKI</Grid>
